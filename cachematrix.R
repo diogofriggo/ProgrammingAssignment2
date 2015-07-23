6f0795942c75955bc3ae353de76abbe78b8d07fd
diogofriggo <diogofriggobr@gmail.com>
2015-07-23 12:44:28 -0300

    Implemented the required functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions work together to avoid repeating computationally intensive code
+## by caching the result once and retrieving it back when needed instead of recalculating it
 
-## Write a short comment describing this function
+## Returns an object (essentially a list) that can hold state
+## It holds a matrix and its inverse, both values can be set and retrieved
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+        inv <- NULL
+        set <- function(x) {
+            m <<- x
+            inv <<- NULL
+        }
+        get <- function() m
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set = set, 
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## It takes a especial matrix made from makeCacheMatrix and returns its cached inverse
+## This functions assumes that the matrix m is always invertible
+cacheSolve <- function(m, ...) {
+    inv <- m$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    inv <- solve(m$get(), ...)
+    m$setinverse(inv)
+    inv
 }
